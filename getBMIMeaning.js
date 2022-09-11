84bc521937f4e36611a388d551e1e5046061da09
thannichasompan <88131261+thannichasompan@users.noreply.github.com>
2022-09-11 14:32:20 +0700

    done

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,18 @@
 const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+  return weight/(height^2)  ;
 }
 function getBMIMeaning(weight, height) {
-  //code here
+ let BMI = calculateBMI(weight, height) 
+  if (BMI> 25.0) {
+    return "Overweight"
+    
+  } else if(BMI< 18.5){
+    return "Underweight"
+  }
+  else{
+    return "Normal weight"
+  }
 }
 module.exports = getBMIMeaning
